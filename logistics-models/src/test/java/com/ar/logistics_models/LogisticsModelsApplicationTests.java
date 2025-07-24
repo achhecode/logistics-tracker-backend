a708e93a70927c4c2eb3482a05387526183fce0f
A R Danish <danish.ar@feuji.com>
2025-07-24 17:50:49 +0530

    logistics-models: service created

---
+++ logistics-models/src/test/java/com/ar/logistics_models/LogisticsModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.logistics_models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
