7229327d252a6899e6e08e58bc40ae58a13f049a
A R Danish <danish.ar@feuji.com>
2025-07-26 04:02:54 +0530

    booking-service mapping entity to dto automatically using mapper library
    tested with API's works fine no need to mannually convert if name are similar we just need to define explicitly for objects which have different name

---
+++ booking-service/src/main/java/com/ar/booking_service/mapper/BookingEntityDTOMapper.java
@@ -0,0 +1,25 @@
+package com.ar.booking_service.mapper;
+
+import com.ar.booking_service.entity.*;
+import com.ar.logistics_models.dto.*;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+@Mapper(componentModel = "spring")
+public interface BookingEntityDTOMapper {
+    BookingDTO toDTO(BookingEntity entity);
+    BookingEntity toEntity(BookingDTO bookingDTO);
+
+    List<GoodsDTO> toDTO(List<GoodsEmbeddable> entities);
+    List<GoodsEmbeddable> toEntities(List<GoodsDTO> dtos);
+
+    AddressDTO toDTO(AddressEmbeddable entity);
+    AddressEmbeddable toEntity(AddressDTO dto);
+
+    PaymentPreferenceDTO toDTO(PaymentPreferenceEmbeddable entity);
+    PaymentPreferenceEmbeddable toEntity(PaymentPreferenceDTO dto);
+
+    ReferralDTO toDTO(ReferralEmbeddable entity);
+    ReferralEmbeddable toEntity(ReferralDTO dto);
+}
+
